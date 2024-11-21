4650479610f674499f30e4addd6e7045377b227e
chachignot <noechachignot@gmail.com>
2024-11-21 14:10:37 +0100

    CPU Ok + format WIP

--- ScriptV2/main.py
+++ ScriptV2/main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from typing import re
 
 from perfetto.trace_processor import TraceProcessor
 from google.protobuf.json_format import MessageToDict
@@ -23,6 +24,24 @@ def create_rail_entry(name, energy_list):
         "delta_list": []
     }
 
+def cpu_freq_compilation(freq_tab):
+    little = 0
+    medium = 0
+    big = 0
+    for key in freq_tab:
+        num = 0
+        den = 0
+        for elt in freq_tab[key]:
+            num += int(elt[0])*int(elt[1])
+            den += int(elt[1])
+        if key == "Little":
+            little = num / den
+        if key == "Medium":
+            medium = num / den
+        if key == "Big":
+            big = num / den
+    return little, medium, big
+
 def energy_delta(list_energy):
     list_delta = []
     timestamps = len(list_energy)
@@ -37,31 +56,21 @@ def energy_delta(list_energy):
 
 def parse_file(filename):
     freq_tab = {
-        "CPU0" : [],
-        "CPU1" : [],
-        "CPU2" : [],
-        "CPU3" : [],
-        "CPU4" : [],
-        "CPU5" : [],
-        "CPU6" : [],
-        "CPU7" : [],
+        "Little" : [],
+        "Medium" : [],
+        "Big" : []
     }
-    core_runtime_total = [0, 0, 0, 0, 0, 0, 0, 0]
-    core_avg_freq_total = [0, 0, 0, 0, 0, 0, 0, 0]
+
+    cpu_little_freq = 0
+    cpu_medium_freq = 0
+    cpu_big_freq = 0
     gpu0_freq = 0
     gpu1_freq = 0
     gpu_mem_avg = 0
 
     tp = TraceProcessor(trace=filename)
 
-    ad_cpu_metrics = tp.metric(['android_cpu'])
-
-    open('./out/tmp', 'w').close()
-    with open('./out/tmp', 'w') as f:
-        print(ad_cpu_metrics, file=f)
-    with open('./out/tmp', 'r') as f:
-        lines = f.readlines()
-
+    # Parse GPU metrics
     ad_gpu_metrics = tp.metric(['android_gpu'])
     gpu_idx = 0
     open('./out/tmp', 'w').close()
@@ -79,11 +88,11 @@ def parse_file(filename):
                         gpu1_freq = int(float(lines[i+4].split(":")[1][1:-2]))
                     gpu_idx += 1
 
-
+    # Parse power rails
     ad_power_rails_metrics = tp.metric(['android_powrails'])
     rails_data = []
-    test = MessageToDict(ad_power_rails_metrics)
-    for elt in test['androidPowrails']['powerRails']:
+    gpu_dict = MessageToDict(ad_power_rails_metrics)
+    for elt in gpu_dict['androidPowrails']['powerRails']:
         list_energy = []
         name = elt.get('name')
         for i in elt['energyData']:
@@ -96,23 +105,77 @@ def parse_file(filename):
         i["delta_list"].pop(0)
 
 
-    #return rails_data, cpu_little_freq, cpu_medium_freq, cpu_big_freq, gpu0_freq, gpu1_freq, gpu_mem_avg
+    # Parse CPU metrics
+    ad_cpu_metrics = tp.metric(['android_cpu'])
+    cpu_dict = MessageToDict(ad_cpu_metrics)
+    for elt in cpu_dict['androidCpu']['processInfo']:
+        for ct in elt['coreType']:
+            if ct['type'] == 'little':
+                freq_tab["Little"].append([ct['metrics']['avgFreqKhz'], ct['metrics']['runtimeNs']])
+            if ct['type'] == 'big':
+                freq_tab["Medium"].append([ct['metrics']['avgFreqKhz'], ct['metrics']['runtimeNs']])
+            if ct['type'] == 'bigger':
+                freq_tab["Big"].append([ct['metrics']['avgFreqKhz'], ct['metrics']['runtimeNs']])
+
+    # Calculate average cpu frequency
+    res =  cpu_freq_compilation(freq_tab)
+    cpu_little_freq = res[0]
+    cpu_medium_freq = res[1]
+    cpu_big_freq = res[2]
+
+    return rails_data, int(cpu_little_freq), int(cpu_medium_freq), int(cpu_big_freq), gpu0_freq, gpu1_freq, gpu_mem_avg #Rounded for comprehsion
+
+def result_to_csv(data):
+    with open('./out/out.csv', 'a', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=' ',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(data)
+
+def process_result(trace_name, data, power_rails_slice):
+    line_elements = trace_name + ";"
+    for elt in data[0]:
+        size = len(elt["delta_list"])
+        percent = int(size / (100 / value))
+        print(percent)
+        if percent > 0:
+            line_elements = line_elements + str(sum(elt["delta_list"][0:percent])) + ";"
+        else:
+            line_elements = line_elements + str(sum(elt["delta_list"][size - percent:])) + ";"
+
+    for elt in data:
+        line_elements = line_elements + elt + ";"
+
+def slice_validation(value):
+    res = False
+    pattern = r"^\[(\d+):(\d+)\]$" # [0:100]
+    match = re.match(pattern, value)
+    if match:
+        res =  True
+    x, y = map(int, match.groups())
+    if not (0 < x < 100 and 0 < y < 100 and x < y):
+            res =  False
+            print('slice format : [x:y] with 0 < x < 100 and 0 < y < 100 and x < y')
+    return res
+
 
 def main(args):
-    filenames = load_input_filename()
-
-    try :
-        value = int(args[0])
-    except ValueError:
+    if not slice_validation(args[0]):
         print("Usage: python3 main.py slice (format : [0-100]")
         sys.exit(0)
+
+    filenames = load_input_filename()
 
     if len(filenames) == 0:
         print("No trace file found")
         sys.exit(0)
 
+    data = []
+
     for elt in filenames:
-        data = parse_file(elt)
+        formatted_data = process_result(elt.split('/'[2]), parse_file(elt), args[0])
+        data.append(formatted_data)
+
+    result_to_csv(data)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
